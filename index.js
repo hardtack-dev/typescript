ba71b8fa07effa41fce6a9264791925d8361dacf
HARDTACK-Dev <rjsdn827@gmail.com>
2021-04-11 23:41:04 +0900

    feat: 2.콘솔로그

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-alert("hello!");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Hardtack", age = 19, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-alert("hello!");
+const name = "Hardtack",
+    age= 19,
+    gender= "male";
+
+
+    const sayHi = (name, age, gender) =>{
+        console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+    };
+
+    sayHi(name, age, gender);
+
+    export {};
